8ee8124c1b16bb89f645d9de8199f46bb9f04398
yasinbeyad <yasinbeyad@gmail.com>
2022-11-17 16:58:55 +0330

    first

---
+++ Assets/Universal Shooter Kit/Demos/Source/Scripts/Portal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GercStudio.USK.Scripts
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class Portal : MonoBehaviour
+    {
+        public Transform destination;
+
+        private float portalTimeout;
+
+        private void OnEnable()
+        {
+            portalTimeout = 3;
+        }
+
+        void Start()
+        {
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+
+        void Update()
+        {
+            portalTimeout += Time.deltaTime;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (portalTimeout > 3)
+                {
+                    portalTimeout = 0;
+                    other.transform.position = destination.position;
+                }
+            }
+        }
+    }
+}
